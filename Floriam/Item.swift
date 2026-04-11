ad7dcfe47ec3bb730c6f2bdaad2fda6e8aeef22c
workingdog <ringowathelet@gmail.com>
2026-04-11 17:40:17 +0900

    Initial Commit

---
+++ Floriam/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Floriam
+//
+//  Created by Ringo Wathelet on 2026/04/11.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
